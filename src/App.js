a3320482f1dcbb45004b0b1e7f267f665d13a02a
isohyeong <so-hyeong@naver.com>
2023-09-21 13:31:29 +0900

    feat: 페이지

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles/reset.css";
 import "./styles/global.css";
 import TopNav from "./components/TopNav/TopNav";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="layout">
       <TopNav />
+      <Home />
     </div>
   );
 }
